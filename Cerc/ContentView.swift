61128a9a9d852c532f18ecd27a1b51565c5c128b
Kevin Romero Peces-Barba <kevinrpb@hotmail.com>
2019-07-19 08:16:37 +0200

    Initial Commit

---
+++ Cerc/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Cerc
+//
+//  Created by Kevin Romero Peces-Barba on 7/19/19.
+//  Copyright © 2019 Kevin Romero Peces-Barba. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
