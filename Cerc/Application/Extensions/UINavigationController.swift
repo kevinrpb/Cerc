f667729367b2db884bdec6f3fa58ddda5cced455
Kevin Romero Peces-Barba <kevinrpb@hotmail.com>
2021-09-25 17:43:57 +0200

    Add navigation pop gesturea

---
+++ Cerc/Application/Extensions/UINavigationController.swift
@@ -0,0 +1,19 @@
+//
+//  UINavigationController.swift
+//  Cerc
+//
+//  Created by Kevin Romero Peces-Barba on 25/9/21.
+//
+
+import UIKit
+
+extension UINavigationController: UIGestureRecognizerDelegate {
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
